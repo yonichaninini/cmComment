c911d39393b22b8752fb8efc4b0d2b4724a0f83c
chanmi <cksal5911@naver.com>
2019-12-13 02:27:50 +0900

    comment 작성 reply 작성 수정 삭제 관련 리덕스 모듈 작성

--- front/src/redux/modules/comment.ts
+++ front/src/redux/modules/comment.ts
@@ -1,38 +1,54 @@
 /* Ducks 패턴 으로 작성할거임 */
 import { CommentDataShape } from "../../model/comentShape";
-
+import { UserDataShape } from "../../model/userDataShape";
 /*액션 type 선언*/
 export const ADD_COMMENT = "ADD_COMMENT" as const;
 export const ADD_REPLY = "ADD_REPLY" as const;
 export const UPDATE_COMMENT = "UPDATE_COMMENT" as const;
 export const DELETE_COMMENT = "DELETE_COMMENT" as const;
 
-/*FSA 규칙을 따르는 액션 생성 함수 정의*/
-
+//댓글 등록 대댓글 등록 수정 interface
+interface add_comment {
+  post_id: number;
+  comment: string;
+  user: UserDataShape;
+  creation_time: string;
+  parents_id: number;
+}
+interface add_reply {
+  post_id: number;
+  comment: string;
+  user: UserDataShape;
+  creation_time: string;
+  parents_id: number;
+}
+interface update_comment {
+  comment_id: number;
+  comment: string;
+}
 //댓글 작성
-export const add_comment = (comment: string) => ({
+export const add_comment = ({
+  post_id,
+  comment,
+  user,
+  creation_time
+}: add_comment) => ({
   type: ADD_COMMENT,
-  payload: comment
+  payload: { post_id, comment, user, creation_time }
 });
 //대댓글 작성
 export const add_reply = ({
+  post_id,
   parents_id,
-  comment
-}: {
-  parents_id: number;
-  comment: string;
-}) => ({
+  comment,
+  user,
+  creation_time
+}: add_reply) => ({
   type: ADD_REPLY,
-  payload: { parents_id, comment }
+  payload: { post_id, parents_id, comment, user, creation_time }
 });
 //댓글 수정
-export const update_comment = ({
-  comment_id,
-  comment
-}: {
-  comment_id: number;
-  comment: string;
-}) => ({
+export const update_comment = ({ comment_id, comment }: update_comment) => ({
   type: UPDATE_COMMENT,
   payload: {
     comment,
@@ -45,115 +61,134 @@ export const delete_comment = (comment_id: number) => ({
   payload: comment_id
 });
 
+//action type지정
 type Action =
   | ReturnType<typeof add_comment>
+  | ReturnType<typeof add_reply>
   | ReturnType<typeof update_comment>
   | ReturnType<typeof delete_comment>;
 
-interface initialState {
-  commentData: CommentDataShape[];
-}
-const initialState: initialState = {
-  commentData: [
-    {
-      id: "www.test.com",
-      creation_time: "2019-07-11 오전 11시 30분",
-      post_id: 1,
-      user: {
-        user_id: "cksal5911",
-        loginType: "kakao",
-        user_profile_img:
-          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBhaNckqtbF2gR0kMvWGhwe7ad4BvVAnAcHQqhgiIzp9mXSPXv4Q&s",
-        nick_name: "윤이찬미"
-      },
+//초깃값지정
+type initialState = CommentDataShape[];
 
-      comment_id: 1,
-      comment: "댓글테스트 기능",
-      children: [
-        {
-          id: "www.test.com",
-          creation_time: "2019-10-11 오전 08시 30분",
-          post_id: 1,
-          user: {
-            user_id: "wldud5911",
-            loginType: "kakao",
-            user_profile_img:
-              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBhaNckqtbF2gR0kMvWGhwe7ad4BvVAnAcHQqhgiIzp9mXSPXv4Q&s",
-            nick_name: "민지"
-          },
-          comment_id: 3,
-          comment: "찬미한테 대댓글테스트 기능",
-          parents: 1
-        },
-        {
-          id: "www.test.com",
-          creation_time: "2019-10-11 오전 08시 30분",
-          post_id: 1,
-          user: {
-            user_id: "jojo5911",
-            loginType: "kakao",
-            user_profile_img:
-              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBhaNckqtbF2gR0kMvWGhwe7ad4BvVAnAcHQqhgiIzp9mXSPXv4Q&s",
-            nick_name: "주영"
-          },
-          comment_id: 4,
-          comment: "찬미한테 대댓글테스트 기능",
-          parents: 1
-        }
-      ]
+const initialState: initialState = [
+  {
+    comment_id: 1,
+    creation_time: "2019-07-11 오전 11시 30분",
+    post_id: 1,
+    user: {
+      user_id: "cksal5911",
+      loginType: "kakao",
+      user_profile_img:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBhaNckqtbF2gR0kMvWGhwe7ad4BvVAnAcHQqhgiIzp9mXSPXv4Q&s",
+      nick_name: "윤이찬미"
     },
-    {
-      id: "www.test.com",
-      creation_time: "2019-10-11 오전 03시 30분",
-      post_id: 1,
-      user: {
-        user_id: "jojo5911",
-        loginType: "kakao",
-        user_profile_img:
-          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBhaNckqtbF2gR0kMvWGhwe7ad4BvVAnAcHQqhgiIzp9mXSPXv4Q&s",
-        nick_name: "주영"
+    comment: "댓글테스트 기능",
+    children: [
+      {
+        comment_id: 3,
+        creation_time: "2019-10-11 오전 08시 30분",
+        post_id: 1,
+        user: {
+          user_id: "wldud5911",
+          loginType: "kakao",
+          user_profile_img:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBhaNckqtbF2gR0kMvWGhwe7ad4BvVAnAcHQqhgiIzp9mXSPXv4Q&s",
+          nick_name: "민지"
+        },
+        comment: "찬미한테 대댓글테스트 기능",
+        parents: 1
       },
-      comment_id: 2,
-      comment: "댓글테스트 기능",
-      children: [
-        {
-          id: "www.test.com",
-          creation_time: "2019-10-11 오전 08시 30분",
-          post_id: 1,
-          user: {
-            user_id: "jiwoo5911",
-            loginType: "kakao",
-            user_profile_img:
-              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBhaNckqtbF2gR0kMvWGhwe7ad4BvVAnAcHQqhgiIzp9mXSPXv4Q&s",
-            nick_name: "지우"
-          },
-          comment_id: 8,
-          comment: "희수한테 대댓글테스트 기능",
-          parents: 1
-        }
-      ]
-    }
-  ]
-};
+      {
+        comment_id: 4,
+        creation_time: "2019-10-11 오전 08시 30분",
+        post_id: 1,
+        user: {
+          user_id: "jojo5911",
+          loginType: "kakao",
+          user_profile_img:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBhaNckqtbF2gR0kMvWGhwe7ad4BvVAnAcHQqhgiIzp9mXSPXv4Q&s",
+          nick_name: "주영"
+        },
+        comment: "찬미한테 대댓글테스트 기능",
+        parents: 1
+      }
+    ]
+  },
+  {
+    comment_id: 2,
+    creation_time: "2019-10-11 오전 03시 30분",
+    post_id: 1,
+    user: {
+      user_id: "jojo5911",
+      loginType: "kakao",
+      user_profile_img:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBhaNckqtbF2gR0kMvWGhwe7ad4BvVAnAcHQqhgiIzp9mXSPXv4Q&s",
+      nick_name: "주영"
+    },
+    comment: "댓글테스트 기능",
+    children: [
+      {
+        comment_id: 8,
+        creation_time: "2019-10-11 오전 08시 30분",
+        post_id: 1,
+        user: {
+          user_id: "jiwoo5911",
+          loginType: "kakao",
+          user_profile_img:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBhaNckqtbF2gR0kMvWGhwe7ad4BvVAnAcHQqhgiIzp9mXSPXv4Q&s",
+          nick_name: "지우"
+        },
+        comment: "희수한테 대댓글테스트 기능",
+        parents: 1
+      }
+    ]
+  }
+];
 
 function comment(state: initialState = initialState, action: Action) {
   switch (action.type) {
     case ADD_COMMENT:
-      return {
-        ...state
-      };
-  }
-  switch (action.type) {
+      const nextCommentId = Math.max(...state.map(c => c.comment_id)) + 1;
+      return state.push({
+        comment_id: nextCommentId,
+        creation_time: action.payload.creation_time,
+        post_id: action.payload.post_id,
+        user: action.payload.user,
+        children: [],
+        comment: action.payload.comment
+      });
+    case ADD_REPLY:
+      const nextReplyId = Math.max(...state.map(c => c.comment_id)) + 1;
+      const replyLessState = state.filter(
+        r => r.comment_id !== action.payload.parents_id
+      );
+      const replyObject = state.find(
+        r => r.comment_id === action.payload.parents_id
+      );
+      if (replyObject) {
+        replyObject.children.push({
+          comment_id: nextReplyId,
+          creation_time: action.payload.creation_time,
+          post_id: action.payload.post_id,
+          user: action.payload.user,
+          comment: action.payload.comment,
+          parents: action.payload.parents_id
+        });
+      }
+      return [...replyLessState, replyObject];
+
     case UPDATE_COMMENT:
       return {
         ...state
       };
-  }
-  switch (action.type) {
+
     case DELETE_COMMENT:
-      return {
-        ...state
-      };
+      return state.filter(
+        commentData => commentData.comment_id !== action.payload
+      );
+    default:
+      return state;
   }
 }
 export default comment;
