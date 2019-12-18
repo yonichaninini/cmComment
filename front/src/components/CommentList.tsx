1d898e40d1e669d925e7f1e4db04bc858ec5545f
chanmi <cksal5911@naver.com>
2019-12-19 03:42:39 +0900

    prettier 설정 바꾸니 자동으로 바뀌어짐

--- front/src/components/CommentList.tsx
+++ front/src/components/CommentList.tsx
@@ -1,11 +1,11 @@
-import * as React from "react";
-import "../styles/comment.scss";
+import * as React from 'react';
+import '../styles/comment.scss';
 
-import CommentItem from "./CommentItem";
-import ProfileImg from "./ProfileImg";
+import CommentItem from './CommentItem';
+import ProfileImg from './ProfileImg';
 
-import { CommentDataShape } from "../typeShapes/comentShape";
-import { UserDataShape } from "../typeShapes/userDataShape";
+import { CommentDataShape } from '../typeShapes/comentShape';
+import { UserDataShape } from '../typeShapes/userDataShape';
 
 interface CommentListProps {
   commentData: CommentDataShape[];
@@ -20,14 +20,7 @@ const CommentList = ({ commentData, userData }: CommentListProps) => {
             <ProfileImg profile_img={c.user.user_profile_img} />
           </div>
           <div className="right">
-            <CommentItem
-              creation_time={c.creation_time}
-              user_id={c.user.user_id}
-              nick_name={c.user.nick_name}
-              comment_id={c.comment_id}
-              comment={c.comment}
-              children={c.children}
-            />
+            <CommentItem creation_time={c.creation_time} user_id={c.user.user_id} nick_name={c.user.nick_name} comment_id={c.comment_id} comment={c.comment} children={c.children} />
           </div>
         </div>
         {c.children.map(r => {
@@ -37,13 +30,7 @@ const CommentList = ({ commentData, userData }: CommentListProps) => {
                 <ProfileImg profile_img={r.user.user_profile_img} />
               </div>
               <div className="right">
-                <CommentItem
-                  creation_time={r.creation_time}
-                  user_id={r.user.user_id}
-                  nick_name={r.user.nick_name}
-                  comment_id={r.comment_id}
-                  comment={r.comment}
-                />
+                <CommentItem creation_time={r.creation_time} user_id={r.user.user_id} nick_name={r.user.nick_name} comment_id={r.comment_id} comment={r.comment} />
               </div>
             </div>
           );
