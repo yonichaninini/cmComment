29ecd5031d39fd553ae03a06b64cecff3981da88
chanmi <cksal5911@naver.com>
2020-01-08 08:20:58 +0900

    interface변경

--- front/src/components/CommentList/CommentList.tsx
+++ front/src/components/CommentList/CommentList.tsx
@@ -11,26 +11,27 @@ interface CommentListProps {
   commentData: CommentDataShape[];
   userData: UserDataShape;
 }
+
 const CommentList = ({ commentData, userData }: CommentListProps) => {
   const comment = commentData.map(c => {
     return (
       <>
         <div className="comment" key={c._id}>
           <div className="left">
-            <ProfileImg profile_img={c.user.user_profile_img} />
+            <ProfileImg />
           </div>
           <div className="right">
-            <CommentItem create_time={c.create_time} update_time={c.update_time} user_id={c.user.user_id} nick_name={c.user.nick_name} _id={c._id} comment={c.comment} children={c.children} />
+            <CommentItem create_time={c.create_time} _id={c._id} comment={c.comment} isparents={true} />
           </div>
         </div>
         {c.children.map(r => {
           return (
             <div className="reply hidden" key={r._id}>
               <div className="left">
-                <ProfileImg profile_img={r.user.user_profile_img} />
+                <ProfileImg />
               </div>
               <div className="right">
-                <CommentItem create_time={r.create_time} update_time={r.update_time} user_id={r.user.user_id} nick_name={r.user.nick_name} _id={r._id} comment={r.comment} />
+                <CommentItem create_time={r.create_time} _id={r._id} comment={r.comment} isparents={false} />
               </div>
             </div>
           );
--- front/src/typeShapes/userDataShape.ts
+++ front/src/typeShapes/userDataShape.ts
@@ -1,6 +1,4 @@
 export interface UserDataShape {
-  user_id: string;
-  loginType: string;
-  user_profile_img: string;
-  nick_name: string;
+  user_email: string;
+  user_password: string;
 }
