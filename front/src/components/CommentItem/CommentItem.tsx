d9e628c6d22157c692078b20d1ef6ef25828c4e3
chanmi <cksal5911@naver.com>
2020-01-15 23:37:36 +0900

    commentItem component에 박혀 있던 모달창 분리

--- front/src/components/CommentItem/CommentItem.tsx
+++ front/src/components/CommentItem/CommentItem.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
 import { useState } from 'react';
 
-import { ReplyDataShape } from '../../typeShapes/comentShape';
+import './commentItem.scss';
+
 import CommentInput from '../CommentInput/CommentInput';
 import { dateFormat } from '../../utils/dateFormat';
-import { CommentManage } from '../../models/CommentManage';
+import Modal from '../Modal/Modal';
 
 interface ComentProps {
   _id: string;
@@ -15,29 +16,22 @@ interface ComentProps {
 
 const Comment = ({ _id, comment, create_time, isparents }: ComentProps) => {
   const [isShowCommentInputBox, SetIsShowCommentInputBox] = useState(false);
-  const [password, setPassword] = useState('');
   const [isOpenModal, setIsOpenModal] = useState(false);
+
   const onClickShowCommentInput = (e: React.MouseEvent<HTMLElement>) => {
     SetIsShowCommentInputBox(!isShowCommentInputBox);
     //TODO
   };
-  const commentManage = new CommentManage('1');
-  const create_date = dateFormat(String(create_time));
-
   const deleteBtnClick = (e: React.FormEvent<HTMLElement>) => {
     e.preventDefault();
     setIsOpenModal(true);
   };
-  const onChangePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setPassword(e.currentTarget.value);
-  };
-  const onSubmitPasswordCheck = (e: React.FormEvent<HTMLElement>) => {
-    commentManage.deleateComment(_id, password);
+  const closeModal = () => {
     setIsOpenModal(false);
   };
-  const CancelBtnClick = (e: React.FormEvent<HTMLElement>) => {
-    setIsOpenModal(false);
-  };
+
+  const create_date = dateFormat(String(create_time));
+
   return (
     <>
       <div className="comment-head">
@@ -45,6 +39,7 @@ const Comment = ({ _id, comment, create_time, isparents }: ComentProps) => {
           <div className="profile-nickname">익명</div>
           <div className="create_time">작성일 : {create_date}</div>
         </div>
+        <Modal _id={_id} isOpen={isOpenModal} close={closeModal} />
         <button className="delete_icon" onClick={deleteBtnClick}>
           삭제
         </button>
@@ -62,17 +57,6 @@ const Comment = ({ _id, comment, create_time, isparents }: ComentProps) => {
         ''
       )}
       {isShowCommentInputBox ? <CommentInput _id={_id} isReply={isShowCommentInputBox} /> : ''}
-      {isOpenModal ? (
-        <form onSubmit={onSubmitPasswordCheck}>
-          <input type="password" placeholder="댓글 작성시 입력한 비밀번호를 적어주세요" onChange={onChangePassword}></input>
-          <button type="submit">확인</button>
-          <button type="button" onClick={CancelBtnClick}>
-            취소
-          </button>
-        </form>
-      ) : (
-        ''
-      )}
     </>
   );
 };
